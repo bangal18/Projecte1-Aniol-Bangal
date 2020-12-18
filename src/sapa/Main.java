32a102032f55bc87d834fd809ee130539833b699
aniolbc <aniolbc.ppp@hotmail.com>
2020-12-18 09:42:53 +0100

    ccc

---
+++ src/sapa/Main.java
@@ -0,0 +1,10 @@
+package sapa;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Aooo");
+        System.out.println("UwU");
+	// write your code here
+    }
+}
