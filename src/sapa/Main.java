55d66f95c7d47ce892fa2b3952c0b8ad3f884838
bangal18 <74682095+bangal18@users.noreply.github.com>
2020-12-18 10:24:36 +0100

    Update Main.java

--- src/sapa/Main.java
+++ src/sapa/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Aooo");
         System.out.println("UwU");
+	System.out.println("ok"); 
 	// write your code here
     }
 }
